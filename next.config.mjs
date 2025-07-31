0dcd9ea681dcc4078298e8066d145a5f3d9b201d
mupseyyy <adrianaisovic@gmail.com>
2025-07-31 19:32:59 +0200

    KKK
    KKK

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.builder.io',
+        port: '',
+        pathname: '/api/v1/image/assets/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
